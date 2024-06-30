ed4d60e8e9a344a2bb5e633f501db05cebf35e9e
mohbscode <mohbscode@gmail.com>
2024-06-30 06:18:52 +0100

    created a function that plays sound | added more buttons

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,11 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSound(int noteNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$noteNumber.wav'));
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -17,19 +22,94 @@ class XylophoneApp extends StatelessWidget {
           title: const Text('Xylophone'),
         ),
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: const Text(
-                'SOUND',
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  playSound(1);
+                },
+                child: Container(
+                  width: 100.0,
+                  height: 50.0,
+                  color: Colors.red,
+                ),
               ),
-            ),
+              TextButton(
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Container(
+                  width: 100.0,
+                  height: 50.0,
+                  color: Colors.orange,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Container(
+                  width: 100.0,
+                  height: 50.0,
+                  color: Colors.green,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Container(
+                  width: 100.0,
+                  height: 50.0,
+                  color: Colors.blue,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Container(
+                  width: 100.0,
+                  height: 50.0,
+                  color: Colors.yellow,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Container(
+                  width: 100.0,
+                  height: 50.0,
+                  color: Colors.teal,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Container(
+                  width: 100.0,
+                  height: 50.0,
+                  color: Colors.brown,
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+// child: Center(
+//             child: TextButton(
+//               onPressed: () {
+//                 final player = AudioPlayer();
+//                 player.play(AssetSource('note1.wav'));
+//               },
+//               child: const Text(
+//                 'SOUND',
+//               ),
+//             ),
+//           ),
