328d2c6888e6c46bb1908d170ebd1747ec401e3a
mohbscode <mohbscode@gmail.com>
2024-07-01 14:30:16 +0100

    refactored code

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,25 @@ class XylophoneApp extends StatelessWidget {
     player.play(AssetSource('note$noteNumber.wav'));
   }
 
+  Expanded buildKey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Container(
+          constraints: const BoxConstraints.expand(),
+          color: color,
+          child: const Icon(
+            Icons.circle,
+            size: 30.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -20,128 +39,19 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         // appBar: AppBar(
-        //   title: const Text('Xylophone'),
+        // title: const Text('Xylophone'),
         // ),
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: Container(
-                    constraints: const BoxConstraints.expand(),
-                    color: Colors.red,
-                    child: const Icon(
-                      Icons.circle,
-                      size: 30.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: Container(
-                    constraints: const BoxConstraints.expand(),
-                    color: Colors.orange,
-                    child: const Icon(
-                      Icons.circle,
-                      size: 30.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: Container(
-                    constraints: const BoxConstraints.expand(),
-                    color: Colors.green,
-                    child: const Icon(
-                      Icons.circle,
-                      size: 30.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: Container(
-                    constraints: const BoxConstraints.expand(),
-                    color: Colors.blue,
-                    child: const Icon(
-                      Icons.circle,
-                      size: 30.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: Container(
-                    constraints: const BoxConstraints.expand(),
-                    color: Colors.yellow,
-                    child: const Icon(
-                      Icons.circle,
-                      size: 30.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  child: Container(
-                    constraints: const BoxConstraints.expand(),
-                    color: Colors.teal,
-                    child: const Center(
-                      child: Icon(
-                        Icons.circle,
-                        size: 30.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: Container(
-                    constraints: const BoxConstraints.expand(),
-                    color: Colors.brown,
-                    child: const Center(
-                      child: Icon(
-                        Icons.circle,
-                        size: 30.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.green, soundNumber: 3),
+              buildKey(color: Colors.blue, soundNumber: 4),
+              buildKey(color: Colors.yellow, soundNumber: 5),
+              buildKey(color: Colors.teal, soundNumber: 6),
+              buildKey(color: Colors.indigo, soundNumber: 7),
             ],
           ),
         ),
@@ -149,15 +59,3 @@ class XylophoneApp extends StatelessWidget {
     );
   }
 }
-
-// child: Center(
-//             child: TextButton(
-//               onPressed: () {
-//                 final player = AudioPlayer();
-//                 player.play(AssetSource('note1.wav'));
-//               },
-//               child: const Text(
-//                 'SOUND',
-//               ),
-//             ),
-//           ),
